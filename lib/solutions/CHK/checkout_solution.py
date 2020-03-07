62eebce981e369126abdad06348f0e826ae7ae78
sbx_user1051 <sbx_user1051@169.254.10.165>
2020-03-07 07:29:23 +0000

    Sat Mar 07 07:29:23 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -7,25 +7,29 @@ def checkout(skus):
     refined_list = []
     print(item_list)
     for item in item_list:
-    	num = 0
+    	num = -1
     	it = None
     	print(item)
     	for s in item:
     		
     		if s.isdigit():
+    			if(num == -1):
+    				num = 0
     			num = num*10 + int(s)
-    		elif ((s == 'A' or s == 'B' or s == 'C' or s == 'D') and it == None):
+    		elif ((s == 'A' or s == 'B' or s == 'C' or s == 'D') and it == None and num != -1):
     			it = s 
     		else:
     			print(s)
     			return -1
-
+    	if(num < 0):
+    		return -1
     	refined_list.append([num,it])
 
     print(item_list)
     print(refined_list)
 
-print(checkout("1A 2B"))
+print(checkout("1A 3A"))
+
 
 
 
