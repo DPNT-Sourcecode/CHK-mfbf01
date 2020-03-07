9fea717359851b3056f7b18da767f28b48ce92ad
sbx_user1051 <sbx_user1051@169.254.10.165>
2020-03-07 09:04:04 +0000

    Sat Mar 07 09:04:04 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -82,7 +82,7 @@ def first_clear(d):
 	return [d,p]
 	
 def second_clear(d,p,price_d):
-	for x in list(d):
+	for x in list(d.keys()):
 		if(d[x] >= 0):
 			p += d[x] * price_d[x]
 
@@ -144,6 +144,7 @@ def checkout(skus):
     return p
 
 print(checkout("ZXXA"))
+
 
 
 
