3ff396362add3b6a4fb5c023e5f0999ba36fb531
sbx_user1051 <sbx_user1051@169.254.10.165>
2020-03-07 07:33:44 +0000

    Sat Mar 07 07:33:44 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,11 +5,13 @@
 def checkout(skus):
     item_list = skus.split(" ")
     refined_list = []
-    print(item_list)
+    #print(item_list)
+
+    #parse skus
     for item in item_list:
     	num = -1
     	it = None
-    	print(item)
+    	#print(item)
     	for s in item:
     		
     		if s.isdigit():
@@ -19,16 +21,27 @@ def checkout(skus):
     		elif ((s == 'A' or s == 'B' or s == 'C' or s == 'D') and it == None and num != -1):
     			it = s 
     		else:
-    			print(s)
+    			#print(s)
     			return -1
     	if(num < 0):
     		return -1
     	refined_list.append([num,it])
 
-    print(item_list)
-    print(refined_list)
+    price = 0
+    for x in refined_list:
+    	if(x[1] == 'A'):
+    		price += x[0]//3 * 130 + x[0]%3 * 50
+    	elif(x[1] == 'B'):
+    		price += x[0]//2 * 45 + x[0]%2 * 30
+    	elif(x[1] == 'C'):
+    		price += 20
+    	elif(x[1] == 'D'):
+    		price += 15
 
-print(checkout("1A 3A"))
+    return price
+
+print(checkout("4A 3B"))
+
 
 
 
