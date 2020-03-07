86b899df56956c42a77577515804ab8577000f1a
sbx_user1051 <sbx_user1051@169.254.10.165>
2020-03-07 08:04:09 +0000

    Sat Mar 07 08:04:09 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,31 +2,51 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+def price_dict():
+	p = {}
+	p['A'] = 50
+	p['B'] = 30
+	p['C'] = 20
+	p['D'] = 15
+	p['E'] = 40
+	p['F'] = 10
+	p['G'] = 20
+	p['H'] = 10
+	p['I'] = 35
+	p['J'] = 60
+	p['K'] = 80
+	p['L'] = 90
+	p['M'] = 15
+	p['N'] = 40
+	p['O'] = 10
+	p['P'] = 50
+	p['Q'] = 30
+	p['R'] = 50
+	p['S'] = 30
+	p['T'] = 20
+	p['U'] = 40
+	p['V'] = 50
+	p['W'] = 20
+	p['X'] = 90
+	p['Y'] = 10
+	p['Z'] = 50
+
+
 def checkout(skus):
     item_list = skus.split(" ")
-    refined_list = [0,0,0,0,0,0]
-    #print(item_list)
-
-    #parse skus
+    item_dict = {}
+    price_dict = price_dict()
+    
     for item in item_list:
     	num = -1
     	it = None
-    	#print(item)
     	for s in item:
-    		
-    		
-    		if(s=='A'):
-    			refined_list[0] += 1
-    		elif(s=='B'):
-    			refined_list[1] += 1
-    		elif(s=='C'):
-    			refined_list[2] += 1
-    		elif(s=='D'):
-    			refined_list[3] += 1
-    		elif(s=='E'):
-    			refined_list[4] += 1
-    		elif(s=='F'):
-    			refined_list[5] += 1
+    		if(s.isupper()):
+    			if(s not in item_dict):
+    				item_dict[s] = 1
+    			else:
+    				item_dict[s] += 1
     		else:
     			return -1
     if(refined_list[5] >2):
@@ -49,4 +69,5 @@ def checkout(skus):
     return price
 
 print(checkout("FFFFF"))
+
 
