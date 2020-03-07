77c6ea8b7a4ffb433b9b10eeb6eced86a8af084a
sbx_user1051 <sbx_user1051@169.254.10.165>
2020-03-07 07:23:22 +0000

    Sat Mar 07 07:23:22 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -4,8 +4,25 @@
 # skus = unicode string
 def checkout(skus):
     item_list = skus.split(" ")
+    refined_list = []
+    for item in item_list:
+    	num = 0
+    	item = None
+
+    	for s in item_list:
+    		print(s)
+    		if s.isdigit():
+    			num = num*10 + int(s)
+    		elif ((s == 'A' or s == 'B' or s == 'C' or s == 'D') and s == None):
+    			item = s 
+    		else:
+    			return -1
+
+    	refined_list.append([num,item])
 
     print(item_list)
+    print(refined_list)
 
 print(checkout("1A 2B"))
+
 
